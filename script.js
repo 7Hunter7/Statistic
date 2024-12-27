76d8fe1c12657455a1b2b4dcec8846d88387ae05
Hunter <i.am.hunter.777.ih@gmail.com>
2024-12-27 13:22:31 +0300

    Updated 'script.js' - Added functions 'calculatePercentageChange' and 'formatNumber'. Updated functions 'createTableRows' and 'handleRowClick'

--- script.js
+++ script.js
@@ -2,16 +2,43 @@
 const tableBody = document.getElementById("tableBody");
 const chartContainer = document.getElementById("chartContainer");
 
+// Функция для расчета процентного изменения
+function calculatePercentageChange(current, yesterday) {
+  if (yesterday === 0) {
+    return current === 0 ? "0%" : "∞"; // Обработка деления на ноль и 0/0
+  }
+  const change = ((current - yesterday) / yesterday) * 100;
+  return `${change > 0 ? "+" : ""}${change.toFixed(0)}%`;
+}
+
+// Функция для форматирования чисел
+function formatNumber(number) {
+  return new Intl.NumberFormat("ru-RU").format(number);
+}
+
 // Функция для создания строк таблицы на основе данных из tableData
 function createTableRows() {
   tableData.forEach((row, index) => {
     const tr = document.createElement("tr");
+    // Условное добавление классов к ячейкам для отображения цвета
+    const percentageChange = calculatePercentageChange(
+      row.currentDay,
+      row.yesterday
+    );
+    let changeClass = "no-change";
+    if (percentageChange.startsWith("+")) {
+      changeClass = "positive-change";
+    } else if (percentageChange.startsWith("-")) {
+      changeClass = "negative-change";
+    }
     tr.innerHTML = `
-            <td>${row.indicator}</td>
-            <td>${row.currentDay}</td>
-            <td>${row.yesterday}</td>
-            <td>${row.thisDayLastWeek}</td>
-        `;
+    <td>${row.indicator}</td>
+    <td>${formatNumber(row.currentDay)}</td>
+    <td class="${changeClass}">${formatNumber(
+      row.yesterday
+    )} <span style="float: right;">${percentageChange}</span></td>
+    <td>${formatNumber(row.thisDayLastWeek)}</td>
+    `;
     tr.addEventListener("click", () => handleRowClick(row.data, row.indicator));
     tableBody.appendChild(tr);
   });
@@ -20,7 +47,10 @@ function createTableRows() {
 // Функция для обработки клика по строке таблицы и отображения графика.
 function handleRowClick(data, indicator) {
   chartContainer.style.display = "block";
-  Highcharts.chart(chartContainer, {
+  if (chart) {
+    chart.destroy();
+  }
+  chart = Highcharts.chart(chartContainer, {
     title: {
       text: `График для показателя "${indicator}"`,
     },
